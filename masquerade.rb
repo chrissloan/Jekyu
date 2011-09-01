ab6fd3c56de54151866b4bc74dc450233061cf2d
Chris Sloan <chris@chrissloan.info>
2011-08-31 21:14:34 -0400

    Adding initial files

---
+++ masquerade.rb
@@ -0,0 +1,21 @@
+# From http://jstorimer.com/2009/12/29/jekyll-on-heroku.html with modifications
+
+require 'rubygems'
+require 'sinatra'
+
+set :public, Proc.new { File.join(root, "_site") }
+
+# This before filter ensures that your pages are only ever served 
+# once (per deploy) by Sinatra, and then by Varnish after that
+before do
+  response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
+end
+
+get '/' do
+  File.read('_site/index.html')
+end
+
+post '/' do
+  File.read('_site/index.html')
+end
+
